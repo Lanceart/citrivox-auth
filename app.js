bd6b5cc3b372833b74206eb63c6a7b37c0a5a85f
Linqing <62589604+Lanceart@users.noreply.github.com>
2024-06-21 16:28:37 -0700

    step://token sys

--- app.js
+++ app.js
@@ -6,21 +6,137 @@ const jwkToPem = require('jwk-to-pem');
 const axios = require('axios');
 
 
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
+
+
+// Fetch JWKS once at startup
+let jwks;
+let pem;
+
+const fetchJwks = async () => {
+  const url = `https://cognito-idp.us-west-1.amazonaws.com/us-west-1_NNbhylbJi/.well-known/jwks.json`;
+  const response = await axios.get(url);
+  jwks = response.data;
+  const jwk = jwks.keys[0];
+  pem = jwkToPem(jwk);
+};
+
+fetchJwks().catch(console.error);
+
+
+
 AWS.config.update({
-    region: 'us-east-1', // Your region
-    accessKeyId: 'YOUR_ACCESS_KEY', // Your access key
-    secretAccessKey: 'YOUR_SECRET_KEY' // Your secret key
+    region: process.env.AWS_DEFAULT_REGION, // Your region
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Your access key
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY // Your secret key
   });
+
+
+const cognito = new AWS.CognitoIdentityServiceProvider();
 
 
 app.get('/',(req,res)=>{
     res.status(200).send('good status')
 })
+
+
+
+  
+//Create user pool
+app.post('/signup', async (req, res) => {
+    // console.log('Signup request body:', req.body);
+    const { username, password, email } = req.body;
+    const params = {
+      ClientId: process.env.AWS_COGNITO_CLIENTID,
+      Username: username,
+      Password: password,
+      UserAttributes: [
+        {
+          Name: 'email',
+          Value: email
+        }
+      ]
+    };
+  
+    try {
+      const data = await cognito.signUp(params).promise();
+      res.status(200).json({ message: 'User signed up', data });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+
+
+  // Request new confirmation code endpoint
+app.post('/request-new-code', (req, res) => {
+    const { username } = req.body;
+
+    const params = {
+        ClientId: process.env.AWS_COGNITO_CLIENTID,
+        Username: username
+    };
+
+    cognito.resendConfirmationCode(params, (err, data) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else {
+            res.status(200).json({ message: 'New confirmation code requested successfully', data });
+        }
+    });
+});
+
+
+
+  app.post('/confirm-signup', (req, res) => {
+    const { username, code } = req.body;
+
+    const params = {
+        ClientId: process.env.AWS_COGNITO_CLIENTID,
+        Username: username,
+        ConfirmationCode: code
+    };
+
+    cognito.confirmSignUp(params, (err, data) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else {
+            res.status(200).json({ message: 'User confirmed successfully', data });
+        }
+    });
+});
+
+
+app.post('/login', async (req, res) => {
+    console.log('Login request body:', req.body); // Debugging statement
+    const { username, password } = req.body;
+    const params = {
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: process.env.AWS_COGNITO_CLIENTID,
+      AuthParameters: {
+        USERNAME: username,
+        PASSWORD: password
+      }
+    };
+  
+    try {
+      const data = await cognito.initiateAuth(params).promise();
+      const { IdToken, AccessToken, RefreshToken } = data.AuthenticationResult;
+      res.status(200).json({
+        message: 'User logged in',
+        idToken: IdToken,
+        accessToken: AccessToken,
+        refreshToken: RefreshToken
+      });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
 
 
 app.listen(port, ()=>{
